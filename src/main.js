20bf9cbe66f62233d3539e48cd1c91c222e96243
erik corkran <erik.corkran@manta.digital>
2025-04-25 20:14:11 -0600

    initial commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(70, innerWidth/innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias:true });
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.IcosahedronGeometry(1, 1);
+const material = new THREE.MeshNormalMaterial();
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+camera.position.z = 3;
+
+// Toggle wireframe mode with 'w' key
+window.addEventListener('keydown', e => {
+  mesh.material.wireframe = e.key === 'w' ? !mesh.material.wireframe : mesh.material.wireframe;
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
