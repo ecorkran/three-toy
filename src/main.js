74333276868306be982a319832c28d2c750092e6
erik corkran <erik.corkran@manta.digital>
2025-04-28 07:35:37 -0600

    Apply CSS styles, fix background issues

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
+import '/src/style.css';
 import * as THREE from 'three';
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(70, innerWidth/innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias:true });
-renderer.setSize(innerWidth, innerHeight);
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000, 1);
 document.body.appendChild(renderer.domElement);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize, false);
+onWindowResize();
 
 const geometry = new THREE.IcosahedronGeometry(1, 1);
 const material = new THREE.MeshNormalMaterial();
 const mesh = new THREE.Mesh(geometry, material);
+scene.background = new THREE.Color(0x000000);
 scene.add(mesh);
 camera.position.z = 3;
 
